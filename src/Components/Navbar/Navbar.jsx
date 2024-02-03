7abc687e15f64d33af0adcd21e1ec58a5c203b2b
Raviegupta <raviegupta153@gmail.com>
2024-02-03 23:42:49 +0530

    responsive Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <div className='navbar'>
         <div className="nav-logo">
             <img src={logo} alt="" />
-            <p>Styler</p>
+            <p>STYLER</p>
         </div>
 
         <ul className='nav-menu'>
