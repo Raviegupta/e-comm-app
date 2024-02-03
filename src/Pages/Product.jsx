c3c191cece103ccdd578e9335348b6d3bdf8ae31
Raviegupta <raviegupta153@gmail.com>
2024-02-03 10:39:42 +0530

    creating Product Page, BreadCrums, ProductDisplay Component

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,22 @@
+import './CSS/Product.css';
+import { ShopContext } from '../Context/ShopContext';
+import { useContext } from 'react';
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
+import { useParams } from 'react-router-dom';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 
 const Product = () => {
+
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrums product={product} />
+      <ProductDisplay product={product} />
+    </div>
   )
 }
 
